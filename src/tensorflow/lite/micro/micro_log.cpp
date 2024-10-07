68c02bfc96de570a9a1bc59eaee6c1452ad73be3
JasonKwok999 <chiwakwok79@gmail.com>
2024-10-07 17:38:12 +0800

    Add existing file

---
+++ src/tensorflow/lite/micro/micro_log.cpp
@@ -0,0 +1,50 @@
+/* Copyright 2018 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#include "tensorflow/lite/micro/micro_log.h"
+
+#include <cstdarg>
+#include <cstdint>
+#include <new>
+
+#if !defined(TF_LITE_STRIP_ERROR_STRINGS)
+#include "tensorflow/lite/micro/micro_string.h"
+#include "tensorflow/lite/micro/debug_log.h"
+#endif
+
+#include "Arduino.h"
+#define DEBUG_SERIAL_OBJECT (Serial)
+
+void Log(const char* format, va_list args) {
+#if !defined(TF_LITE_STRIP_ERROR_STRINGS)
+  // Only pulling in the implementation of this function for builds where we
+  // expect to make use of it to be extra cautious about not increasing the code
+  // size.
+  static constexpr int kMaxLogLen = 256;
+  char log_buffer[kMaxLogLen];
+  MicroVsnprintf(log_buffer, kMaxLogLen, format, args);
+  DEBUG_SERIAL_OBJECT.print(log_buffer);
+  DEBUG_SERIAL_OBJECT.print("\r\n");
+#endif
+}
+
+#if !defined(TF_LITE_STRIP_ERROR_STRINGS)
+void MicroPrintf(const char* format, ...) {
+  va_list args;
+  va_start(args, format);
+  Log(format, args);
+  va_end(args);
+}
+#endif
